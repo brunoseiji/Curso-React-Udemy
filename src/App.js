5910f70bd446b0aa08e4330c63a881c395ca33cf
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2019-12-23 17:04:13 -0300

    Base do curso, testando Props do React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const Bemvindo = (props) => {
+    return(
+        <div>
+            <h2>Bem-vindo(a) {props.nome}</h2>
+            <h3>Tenho {props.idade} anos</h3>
+        </div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div>
+            Olá Mundo!
+            <Bemvindo nome="Bruno" idade="24" />
+            <Bemvindo nome="Paula" idade="21" />
+            <h1>Curso React</h1>
+        </div>
+    )
 }
 
 export default App;
